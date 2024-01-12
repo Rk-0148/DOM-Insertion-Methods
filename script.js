53b93b00389c689207a19854bd610a2b8b77b844
rohan kadam <rohankadam0148@gmail.com>
2024-01-12 15:27:18 +0000

    updated DOM Insertion

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-let a = document.getElementsByClassName('container')[0]
+// lecture 43 : insertion methods in DOM
+
+// let a = document.getElementsByClassName('container')[0]
 // a.innerHTML = a.innerHTML + '<h1>THIS IS INSERTION USING JS METHOD</h1>';
 
-let div = document.createElement('div');
-div.innerHTML = '<h1>THIS IS INSERTION USING JS METHOD</h1>';
+// let div = document.createElement('div');
+// div.innerHTML = '<h1>THIS IS INSERTION USING JS METHOD</h1>';
 // a.appendChild(div);
 // a.append(div);  // append at the end
 // a.prepend(div);  // append at the start
 // a.before(div);   // add innerHTML after passed tag
 // a.after(div);  // add innerHTML after the passed tag
 // a.replaceWith(div);  replaces with existing div element
+
+// lecture 44 : insertAdjacentHTML, insertAdjacentElement and insertAdjacentText
+
+// first.insertAdjacentHTML('beforebegin', '<div class="test">beforebegin</div>');
+// first.insertAdjacentHTML('beforeend', '<div class="test">beforeend</div>');
+// first.insertAdjacentHTML('afterbegin', '<div class="test">afterbegin</div>');
+// first.insertAdjacentHTML('afterend', '<div class="test">afterend</div>');
+// first.remove();
+
+// className and classList
+// first.className = "text-decor yellow";
+// first.classList.remove("yellow");
+// first.className = "text-decor red";
+// first.classList.add("yellow");
+// first.classList.remove("text-decor");
+// document.write(first.classList);
+
+
